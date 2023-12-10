496aecb7c9d2d2f79befd36786e4deed22dde5d0
Jiya Lamba <01fe21bci027@kletech.ac.in>
2023-12-10 10:19:33 +0530

    Changes done

--- Tracker-api/server.js
+++ Tracker-api/server.js
@@ -1,21 +1,25 @@
-const express=require('express');
-const cors=require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
-const mongoose=require('mongoose')      //connection config
-const uri=process.env.ATLAS_URI;
+const mongoose = require('mongoose'); //connection config
+const uri = process.env.ATLAS_URI
+const exercisesRouter = require('./routes/exercises') // add routing files
+const usersRouter = require('./routes/users')
 
-mongoose.connect(uri , {useNewurlParser : true});
+mongoose.connect(uri,{useNewUrlParser:true});
 const connection = mongoose.connection;
 connection.once('open',()=>{
-    console.log('MongoDB is connected');
+    console.log("MongoDB is Connected")
 })
 
 const app = express();
-const port=process.env.PORT || 5000
+const port= process.env.PORT || 5000
 
-app.use(cors())        //middleware
+app.use(cors())  //middleware
 app.use(express.json());
-
+app.use('/exercises',exercisesRouter);// add routing files
+app.use('/users',usersRouter)
+ 
 app.listen(port,()=>{
-    console.log(`server is running on port : ${port}`);
+    console.log(`Server is running on port : ${port}`);
 })
--- Tracker-ui/exercise-tracker/src/App.js
+++ Tracker-ui/exercise-tracker/src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css"
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Exercises from './components/Exercises.component';
+import CreateExercises from './components/CreateExercises.component';
+import Navbar from "./components/NavBar"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Exercises />} />
+        {/* <Route path="/edit/:id" element={<EditExercises />} /> */}
+        <Route path="/create" element={<CreateExercises />} />
+        {/* <Route path="/user" element={<CreateUsers />} /> */}
+        </Routes>
+      </Router>
+    </>
   );
 }
 
